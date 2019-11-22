77b6fddb85a1affafd9a0930ee36e02648e386e1
Amila <akudahewa@gmail.com>
2019-11-22 19:34:48 +0530

    adding demo project

--- InterviewReady/src/java8/LambdaExBetterPractice.java
+++ InterviewReady/src/java8/LambdaExBetterPractice.java
@@ -3,7 +3,7 @@ package java8;
 public class LambdaExBetterPractice {
 
 	public static void main(String args[]) {
-		
+		System.out.println("praticing all my lambda !!!");
 	}
 	
 	interface MathOperation{
