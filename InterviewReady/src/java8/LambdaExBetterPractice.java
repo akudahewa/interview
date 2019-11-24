613b7b17565c4775a9befdc728815b752ff9827e
Amila <akudahewa@gmail.com>
2019-11-24 13:34:51 +0530

    remove =

--- InterviewReady/src/java8/LambdaExBetterPractice.java
+++ InterviewReady/src/java8/LambdaExBetterPractice.java
@@ -3,10 +3,11 @@ package java8;
 public class LambdaExBetterPractice {
 
 	public static void main(String args[]) {
+		System.out.println("============features/feature branch");
 		System.out.println("praticing all my lambda !!!");
 	}
 	
 	interface MathOperation{
 		int operation(int a,int b);
-	}
+	} 	
 }
